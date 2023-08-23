360094bded34b9ee0d974dc5077cfd3c1c60eb8d
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-23 12:11:51 -0300

    Arquvos foto exemplo

--- Layout/app/src/main/java/com/example/layout/MainActivity.java
+++ Layout/app/src/main/java/com/example/layout/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView viImage;
+    Integer[] magess = new Integer[] {
+            R.drawable.gardem,
+            R.drawable.happy
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        viImage=findViewById(R.id.imageView);
+        viImage.setImageResource(R.drawable.patinho);
+        next(view);
+    }
+
+    public  void  next (View v, Integer mages){
+        viImage=findViewById(R.id.imageView);
+        viImage.setImageResource(
+                mages[0]);
     }
 }
