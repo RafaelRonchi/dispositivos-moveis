1b4ada2c82a3faee9e7d5b38f0fd62168a188f1a
rafael <rafael.e01@aluno.ifsc.edu.br>
2023-08-30 11:42:38 -0300

    Imc quase funcionando

--- imc/app/src/main/java/com/example/imc/MainActivity.java
+++ imc/app/src/main/java/com/example/imc/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.imc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public EditText peso;
+    public EditText altura;
+    public Button calcular;
+    public Double alturaD;
+    public Double pesoD;
 
+    public String resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calcular = findViewById(R.id.button);
+        peso = findViewById(R.id.PesoTxt);
+        altura = findViewById(R.id.AlturaTxt);
     }
+
+    public void calcularIMC(View view){
+        pesoD = Double.parseDouble(peso.getText().toString());
+        alturaD = Double.parseDouble(altura.getText().toString());
+
+        resultado = String.valueOf(pesoD / (alturaD * alturaD));
+        Intent i = new Intent(this, Resultado.class);
+        i.putExtra("resultado", resultado);
+        i.putExtra("peso", pesoD);
+        i.putExtra("altura", alturaD);
+        startActivity(i);
+    }
+
+
 }
--- imc/app/src/main/java/com/example/imc/Resultado.java
+++ imc/app/src/main/java/com/example/imc/Resultado.java
@@ -2,13 +2,55 @@ package com.example.imc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
+    public String imc;
+    public TextView resultadoImc;
+    public TextView altura;
+    public TextView peso;
+    public Double alturaR;
+    public Double pesoR;
 
+    public Button voltar;
+
+    public ImageView imagem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        Intent i = getIntent();
+
+        altura = findViewById(R.id.AlturaTxt);
+        peso = findViewById(R.id.PesoTxt);
+        imagem = findViewById(R.id.imageView2);
+        imc = i.getStringExtra("resultado");
+        alturaR = i.getDoubleExtra("altura", 0);
+        pesoR = i.getDoubleExtra("peso", 0);
+
+        resultadoImc = findViewById(R.id.Final);
+        altura.setText("Altura: " +  alturaR );
+        peso.setText("Peso: " + pesoR);
+        resultadoImc.setText(imc);
     }
+
+    public void voltarM(View view){
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void SetarImg(){
+        if (Double.valueOf(imc) <= 18.5){
+            imagem.setImageResource(R.drawable.abaixopeso);
+        }
+        if(Double.valueOf(imc) > 18.5 && Double.valueOf(imc) < 24.9)
+    }
+
 }
