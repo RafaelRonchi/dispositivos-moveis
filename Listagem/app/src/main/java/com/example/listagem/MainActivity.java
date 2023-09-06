bc634173f3a652cbb16fab8db4993e5adc15c5cd
rafael <rafael.e01@aluno.ifsc.edu.br>
2023-09-06 11:14:19 -0300

    List arrayadapter

---
+++ Listagem/app/src/main/java/com/example/listagem/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.listagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    String[] nomes;
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.ListView);
+
+        nomes =new String[] {"Pipoca ", "Cenoura", "Cerveja" };
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+                );
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+            }
+        });
+    }
+}
