97cb6c55bea405ef14d3106151ea338209359f6d
Rafael <rafael.e01@aluno.ifsc.edu.br>
2023-10-18 10:13:44 -0300

    .

--- SQLitTeste/app/src/main/java/com/example/sqlitteste/MainActivity.java
+++ SQLitTeste/app/src/main/java/com/example/sqlitteste/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqlitteste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -12,9 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sqlLitedb = openOrCreateDatabase("meubd", MODE_PRIVATE, null);
-        sqlLitedb.execSQL("CREATE TABLE usr(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        sqlLitedb.execSQL("CREATE TABLE IF NOT EXISTS usr(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "nome VARCHAR);");
-
-
+        ContentValues cv = new ContentValues();
+        cv.put("nome", "Eu");
+        sqlLitedb.insert("usr", null, cv);
     }
 }
