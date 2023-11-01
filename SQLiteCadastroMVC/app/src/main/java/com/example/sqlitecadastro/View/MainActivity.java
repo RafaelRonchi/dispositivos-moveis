ff5b1fcea63dd0162e8cc66d37481aa5bbd57db2
rA <rafael.e01@aluno.ifsc.edu.br>
2023-11-01 11:56:47 -0300

    fazendo

--- SQLiteCadastroMVC/app/src/main/java/com/example/sqlitecadastro/View/MainActivity.java
+++ SQLiteCadastroMVC/app/src/main/java/com/example/sqlitecadastro/View/MainActivity.java
@@ -13,16 +13,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.example.sqlitecadastro.Controller.NotaController;
+import com.example.sqlitecadastro.Model.Nota;
 import com.example.sqlitecadastro.R;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    SQLiteDatabase sqlLitedb;
     ListView lv;
     Button btn;
 
-    EditText text;
+    ArrayList<Nota> lista = new ArrayList<Nota>();
+
+    NotaController _controller;
     @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +33,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lv = findViewById(R.id.listV);
         btn = findViewById(R.id.button);
-        text = findViewById(R.id.editTextTextPersonName);
 
-       listarA();
+        lista = _controller.getListaNotas();
+
     }
 
 
 
-    public void listarA(){
-        Cursor cursor = sqlLitedb.rawQuery("SELECT * FROM pessoas", null);
-        cursor.moveToFirst();
 
-        ArrayList<String> listaUsers = new ArrayList<>();
-        while (!cursor.isAfterLast()){
-            listaUsers.add(cursor.getString(cursor.getColumnIndex("nome")));
-            cursor.moveToNext();
-        }
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                listaUsers
-        );
-
-        lv.setAdapter(arrayAdapter);
-    }
 }
